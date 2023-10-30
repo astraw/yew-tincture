8c6460a65b8e4469f418e0e1b9d88dd42385a268
Andrew Straw <strawman@astraw.com>
2023-10-30 10:24:34 +0100

    'cargo fmt'

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -2,7 +2,7 @@ mod button;
 pub use self::button::Button;
 
 mod typed_input;
-pub use self::typed_input::{TypedInput, TypedInputStorage, RawAndParsed};
+pub use self::typed_input::{RawAndParsed, TypedInput, TypedInputStorage};
 
 mod checkbox_label;
 pub use self::checkbox_label::CheckboxLabel;
